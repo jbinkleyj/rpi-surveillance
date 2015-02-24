c385eaa77e2a156c2ccad0795ed9dbfb500fbab1
Mattias Erming <mattias@mattiaserming.com>
2015-02-24 22:24:16 +0000

    Add socket.io

--- src/server.js
+++ src/server.js
@@ -1,14 +1,22 @@
 var express = require("express");
-var config = require("./config");
+var io = require("socket.io");
 var version = require("../package.json").version;
+var config = require("./config");
 
 module.exports = init;
+
+var sockets;
 
 function init() {
   var port = config("port");
   var app = express()
     .use(express.static("client"))
     .listen(port);
+
+  sockets = io(app);
+  sockets.on("connect", function(s) {
+    console.log(1);
+  });
 
   console.log("");
   console.log("rpi-surveillance@" + version);
