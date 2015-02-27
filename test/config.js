f74ee1b9b1d200288e5cff713bcf04dce0452039
Mattias Erming <mattias@mattiaserming.com>
2015-02-27 00:53:17 +0000

    Test config

--- test/config.js
+++ test/config.js
@@ -1,5 +1,12 @@
 var tape = require("tape");
+var config = require("../src/config");
 
 tape("config", function(t) {
+  var n = config("");
+  var p = config("port");
+
+  t.false(n);
+  t.equal(typeof p, "number");
+  
   t.end();
 });
